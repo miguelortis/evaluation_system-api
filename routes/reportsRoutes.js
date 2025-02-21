ab174ed58e22ca114db28b47fabb97a78a434e25
p-arguelles <pedroarguelles1996@gmail.com>
2025-02-21 13:10:03 -0400

    remove question and report controllers and their associated routes

--- routes/reportsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { generateEmployeeReport } = require("../controllers/reportController");
-const auth = require("../middleware/auth");
-
-router.get("/employee/:id", auth(["admin", "manager"]), generateEmployeeReport);
-
-module.exports = router;
