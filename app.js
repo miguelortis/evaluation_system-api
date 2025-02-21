151e7a0ccf4f8c406a91a8c0a90f002455717fd0
p-arguelles <pedroarguelles1996@gmail.com>
2025-02-21 13:24:11 -0400

    enhance CORS configuration for improved security and flexibility

--- app.js
+++ app.js
@@ -11,13 +11,23 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 // Enable CORS with specific configuration
-app.use(
-  cors({
-    origin: "https://evaluation-system.vercel.app",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 
 const routes = require("./routes");
 app.use("/api", routes);
